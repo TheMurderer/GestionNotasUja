249d5132e7bcc05bc595f7f80db22050e46a6b23
serrano5510 <serrano5510@hotmail.com>
2013-02-12 14:34:53 +0100

    Solucionado jsonp

--- GestionNotasUja/assets/www/apis/login.js
+++ GestionNotasUja/assets/www/apis/login.js
@@ -1,45 +1,26 @@
 function llamadaDatosLogueo(){
 	console.log($('#formRegister').serialize());
 	var p_url = "http://serrano5510.servehttp.com:8443/ServicioWeb/index.php";
-
-	//alert(JSON.stringify($("#formRegister").serializeObject()));
+	
 	var cad = "[" + JSON.stringify($("#formRegister").serializeObject()) + "]";
-	//alert(cad);
-	/* $.get(p_url,
-	    {
-	        'm' : 'reg',
-	        'datos' : cad
-	    },
-	    function(data){
-	        alert('hola');
-	    }
-		);*/
 	
-	function bien(data){
-		alert('guay');
-	}
-	
-	function mal(data){
-		alert('error');
-	}
 	var datas = 'm=reg&datos=' + cad;
 	$.ajax({
 		type: "GET",
 		url: p_url,
+		dataType: 'jsonp',
 		data: {
 			'm':'reg',
 			'datos':cad
 		},
 		contentType:'application/json; charset=utf-8',
-		success: function(datos){
-			alert("funcion");
+		success: function(respuesta){
+			arrayRespuesta = eval(respuesta);
+            alert('RESPUESTA: '+ arrayRespuesta["ok"]);
+		},
+		error: function(respuesta){
+			alert("ERROR, YO NO ENTIENDO PUR KÉ...");
 		}
-		
 	});
 	
-//	$.getJSON(p_url,
-//			{
-//				'm':'reg',
-//				'datos' : cad
-//			},function(data){alert('perfect');});
 }
